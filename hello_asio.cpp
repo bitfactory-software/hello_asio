19e6f6623759085e08035d67c20df63e3184ff4c
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-11-06 22:03:01 +0100

    Add project files.

---
+++ hello_asio.cpp
@@ -0,0 +1,12 @@
+﻿// hello_asio.cpp : Defines the entry point for the application.
+//
+
+#include "hello_asio.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ hello_asio.h
@@ -0,0 +1,8 @@
+﻿// hello_asio.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
